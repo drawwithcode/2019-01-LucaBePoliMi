272036ce543977e3070d304aea4802a438018b7e
LucaBePoliMi <luca.b97@gmail.com>
2019-09-18 12:37:16 +0200

    Prova

--- sketch.js
+++ sketch.js
@@ -4,8 +4,11 @@ function preload(){
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background("red");
 }
 
 function draw() {
+  ellipse(100, 100, 50, 50);
   // put drawing code here
 }
